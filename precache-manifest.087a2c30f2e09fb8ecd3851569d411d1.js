ef238463641ed5af70c831fac7104cc87bd24c4b
Brittany Isenberg <brittany.isenberg@gmail.com>
2019-08-27 10:08:13 -0400

    Updates

---
+++ precache-manifest.087a2c30f2e09fb8ecd3851569d411d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562edfa445927c66483a6ab7d36f0954",
+    "url": "/bandwagon/index.html"
+  },
+  {
+    "revision": "72dcc7c058a9dac1fd2e",
+    "url": "/bandwagon/static/css/main.753cedd9.chunk.css"
+  },
+  {
+    "revision": "97fca9cd1574e02fbdc3",
+    "url": "/bandwagon/static/js/2.1608a74c.chunk.js"
+  },
+  {
+    "revision": "72dcc7c058a9dac1fd2e",
+    "url": "/bandwagon/static/js/main.9148a977.chunk.js"
+  },
+  {
+    "revision": "5d60c5ff6f81fd5c933e",
+    "url": "/bandwagon/static/js/runtime~main.a696ac13.js"
+  },
+  {
+    "revision": "42139d3e41fc12be05375e745d3879a6",
+    "url": "/bandwagon/static/media/gray-bg.42139d3e.jpg"
+  }
+]);
